b4acb14db1f696e5827e6fc2fe6f19d7a5fcc668
Yusuf <easyclick05@gmail.com>
2025-12-12 00:02:27 +0100

    feat: add AutoMigrate logic in database package

--- backend/internal/db/database.go
+++ backend/internal/db/database.go
@@ -2,8 +2,13 @@ package db
 
 import (
 	"context"
+	"database/sql"
 
+	"github.com/deveasyclick/zendo/backend/internal/config"
 	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/pressly/goose/v3"
+	"go.uber.org/zap"
 )
 
 type DB struct {
@@ -19,4 +24,23 @@ func NewDB(ctx context.Context, dbUrl string) (*DB, error) {
 	defer pool.Close()
 
 	return &DB{Pool: pool, Queries: New(pool)}, nil
+}
+
+func AutoMigrate(cfg *config.Config, logger *zap.Logger) error {
+	if cfg.Env != "production" {
+		sqlDB, err := sql.Open("pgx", cfg.DB_URL)
+		if err != nil {
+			return err
+		}
+		defer sqlDB.Close()
+
+		goose.SetDialect("postgres")
+		if err := goose.Up(sqlDB, "migrations"); err != nil {
+			return err
+		}
+		logger.Info("migrations applied successfully")
+	} else {
+		logger.Info("production environment: skipping automatic migrations")
+	}
+	return nil
 }
