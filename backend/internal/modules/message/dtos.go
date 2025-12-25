a810b11681e948ed444c8ecdc59424e71f5eb559
Yusuf <easyclick05@gmail.com>
2025-12-25 23:21:33 +0100

    refactor(message): introduce CreateMessageDTO

--- backend/internal/modules/message/dtos.go
+++ backend/internal/modules/message/dtos.go
@@ -8,11 +8,12 @@ import (
 )
 
 type CreateMessageDTO struct {
-	Content        string `json:"content" validate:"required,min=1"`
-	WebsiteID      int32  `json:"websiteId" validate:"required,gt=0"`
-	VisitorID      string `json:"visitorId" validate:"required"`
-	ConversationID *int32 `json:"conversationId" validate:"omitempty,gt=0"`
-	SenderType     string `json:"senderType" validate:"required,oneof=visitor agent bot"`
+	Content        string  `json:"content" validate:"required,min=1"`
+	WebsiteID      int32   `json:"websiteId" validate:"required,gt=0"`
+	VisitorID      string  `json:"visitorId" validate:"required_if=SenderType visitor"`
+	AgentID        *string `json:"agentId" validate:"required_if=SenderType agent"`
+	ConversationID *int32  `json:"conversationId" validate:"omitempty,gt=0"`
+	SenderType     string  `json:"senderType" validate:"required,oneof=visitor agent bot"`
 }
 
 func ValidateCreateMessageBody(w http.ResponseWriter, r *http.Request) (*CreateMessageDTO, error) {
