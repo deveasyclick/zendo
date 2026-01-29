0e0cb381c66b3457068c58ab6f17a5779e79a8ea
Yusuf <easyclick05@gmail.com>
2026-01-29 15:58:47 +0100

    refactor(user provider): clean up unused user fetching logic and remove TeamRow component

--- frontend/src/contexts/user/provider.tsx
+++ frontend/src/contexts/user/provider.tsx
@@ -1,28 +1,28 @@
 import { type ReactNode } from "react";
 
 import { UserContext } from "./context";
-import { PageLoader } from "@/components/loaders";
-import { useFetchMe } from "@/queries/user";
+// import { PageLoader } from "@/components/loaders";
+// import { useFetchMe } from "@/queries/user";
 
 export function AppUserProvider({
   children,
 }: {
   readonly children: ReactNode;
 }) {
-  const { data: user, isLoading, isError, error: error } = useFetchMe();
+  // const { data: user, isLoading, isError, error: error } = useFetchMe();
 
-  // Clerk or backend is loading
-  if (isLoading) return <PageLoader />;
+  // // Clerk or backend is loading
+  // if (isLoading) return <PageLoader />;
 
-  // Backend error
-  if (isError) {
-    // Optionally sign out if backend fails
-    console.log("fetch user error", error);
-    return <PageLoader />;
-  }
+  // // Backend error
+  // if (isError) {
+  //   // Optionally sign out if backend fails
+  //   console.log("fetch user error", error);
+  //   return <PageLoader />;
+  // }
 
   return (
-    <UserContext.Provider value={{ user: user! }}>
+    <UserContext.Provider value={{ user: {} as any }}>
       {children}
     </UserContext.Provider>
   );
